be21ee8436ccb5794cad9b216e352a3518630186
John Braun <johnbraun@pm.me>
2019-12-08 19:47:01 +0100

    Rename environment variables

--- config/services.php
+++ config/services.php
@@ -2,8 +2,8 @@
 
 return [
     'laravelpassport' => [
-        'client_id' => env('LARAVELPASSPORT_KEY'),
-        'client_secret' => env('LARAVELPASSPORT_SECRET'),
+        'client_id' => env('LARAVELPASSPORT_CLIENT_ID'),
+        'client_secret' => env('LARAVELPASSPORT_CLIENT_SECRET'),
         'redirect' => env('LARAVELPASSPORT_REDIRECT_URI'),
         'host' => env('LARAVELPASSPORT_HOST'),
     ],
